f745e40eb3d91143a60c8c07e415af7867d8fa06
Galio Guo <162270879+galioguo@users.noreply.github.com>
2025-05-07 17:45:01 -0700

    Create reindexing.py

---
+++ reindexing.py
@@ -0,0 +1,250 @@
+import numpy as np
+from math import floor
+from math import ceil
+
+# Simulation Box Settings
+Nx = 8
+Ny = 8
+Nz = 8
+regSize = 5
+Rx = ceil( Nx / regSize )
+Ry = ceil( Ny / regSize )
+Rz = ceil( Nz / regSize )
+
+def findCellFromPos ( pos : int ) :
+	'''
+	pos : global position index encoding type and unit cell
+	----------
+	returns unit cell index
+    '''
+	return ((floor(pos/12)) % (Nx * Ny * Nz))
+
+def findCoordsFromPos ( pos : int ) :
+	'''
+	pos : global position index encoding type and unit cell
+	----------
+	returns x, y, z coordinates of unit cell
+	'''
+	cell = findCellFromPos(pos)
+	nz = floor(cell/(Nx * Ny))
+	plane = cell % (Nx * Ny)
+	ny = floor(plane/Nx)
+	nx = plane % Nx
+	return nx, ny, nz
+
+def findCellFromCoords ( nx : int, ny : int, nz : int ) :
+	'''
+	nx, ny, nz : x, y, z coordinates of unit cell
+	----------
+	returns the unit cell index
+	'''
+	nx = nx % Nx
+	ny = ny % Ny
+	nz = nz % Nz
+	return (nz * Nx * Ny) + (ny * Nx) + nx
+
+def findPosFromType ( cell : int, type: int ) :
+	'''
+	cell : unit cell index 
+	type : integer indexing the type of interstitial (see pdf document)
+	----------
+	returns the global position index of the interstitial site
+	'''
+	type = type % 12
+	return (cell * 12 + type)
+
+def isOctahedral ( pos : int ) :
+	'''
+	pos : global position index encoding type and unit cell
+	----------
+	returns true iff the site is an octahedral
+	'''
+	return ((pos % 12) <= 3)
+
+def findRegionFromPos ( pos : int ) :
+	'''
+	pos : global position index encoding type and unit cell
+	----------
+	returns the region coordinates
+	'''
+	nx, ny, nz = findCoordsFromPos(pos)
+	rx = floor(nx/regSize)
+	ry = floor(ny/regSize)
+	rz = floor(nz/regSize)
+	return rx, ry, rz
+
+def OctNeighbors ( pos : int ) :
+	'''
+	pos : global position index encoding type and unit cell
+	----------
+	returns a numpy array of global indices of nearest neighbors
+	'''
+	type = pos % 12
+	nx, ny, nz = findCoordsFromPos(pos)
+	if (type == 0):
+		mx = (nx - 1) % Nx
+		my = (ny - 1) % Ny
+		mz = (nz - 1) % Nz
+		neighbors = np.array(
+			[
+				# OCTAHEDRAL NEIGHBORS
+				pos + 1,
+				pos + 2,
+				pos + 3, 
+				findPosFromType(findCellFromCoords(mx, ny, nz),1),
+				findPosFromType(findCellFromCoords(mx, ny, nz),2),
+				findPosFromType(findCellFromCoords(nx, my, nz),1),
+				findPosFromType(findCellFromCoords(nx, my, nz),3),
+				findPosFromType(findCellFromCoords(nx, ny, mz),2),
+				findPosFromType(findCellFromCoords(nx, ny, mz),3),
+				findPosFromType(findCellFromCoords(nx, my, mz),3),
+				findPosFromType(findCellFromCoords(mx, ny, mz),2),
+				findPosFromType(findCellFromCoords(mx, my, nz),1),
+				# TETRAHEDRAL NEIGHBORS
+				pos + 4, 
+				pos + 5, 
+				pos + 6, 
+				pos + 7, 
+				pos + 8, 
+				pos + 9, 
+				pos + 10, 
+				pos + 11
+			]
+		)
+	if (type == 1):
+		mx = (nx + 1) % Nx
+		my = (ny + 1) % Ny
+		mz = (nz - 1) % Nz
+		neighbors = np.array(
+			[
+				# OCTAHEDRAL NEIGHBORS
+				pos - 1, 
+				pos + 1, 
+				pos + 2,
+				findPosFromType(findCellFromCoords(mx, ny, nz),0),
+				findPosFromType(findCellFromCoords(mx, ny, nz),3),
+				findPosFromType(findCellFromCoords(nx, my, nz),0),
+				findPosFromType(findCellFromCoords(nx, my, nz),2),
+				findPosFromType(findCellFromCoords(nx, ny, mz),2),
+				findPosFromType(findCellFromCoords(nx, ny, mz),3),
+				findPosFromType(findCellFromCoords(nx, my, mz),2),
+				findPosFromType(findCellFromCoords(mx, ny, mz),3),
+				findPosFromType(findCellFromCoords(mx, my, nz),0),
+				# TETRAHEDRAL NEIGHBORS
+				pos - type + 7, 
+				pos - type + 11,
+				findPosFromType(findCellFromCoords(mx, ny, nz),5),
+				findPosFromType(findCellFromCoords(mx, ny, nz),9),
+				findPosFromType(findCellFromCoords(nx, my, nz),6),
+				findPosFromType(findCellFromCoords(nx, my, nz),10),
+				findPosFromType(findCellFromCoords(mx, my, nz),4),
+				findPosFromType(findCellFromCoords(mx, my, nz),8)
+			]
+		)
+	if (type == 2):
+		mx = (nx + 1) % Nx
+		my = (ny - 1) % Ny
+		mz = (nz + 1) % Nz
+		neighbors = np.array(
+			[
+				# OCTAHEDRAL NEIGHBORS
+				pos - 2, 
+				pos - 1, 
+				pos + 1,
+				findPosFromType(findCellFromCoords(mx, ny, nz),0),
+				findPosFromType(findCellFromCoords(mx, ny, nz),3),
+				findPosFromType(findCellFromCoords(nx, my, nz),1),
+				findPosFromType(findCellFromCoords(nx, my, nz),3),
+				findPosFromType(findCellFromCoords(nx, ny, mz),0),
+				findPosFromType(findCellFromCoords(nx, ny, mz),1),
+				findPosFromType(findCellFromCoords(nx, my, mz),1),
+				findPosFromType(findCellFromCoords(mx, ny, mz),0),
+				findPosFromType(findCellFromCoords(mx, my, nz),3),
+				# TETRAHEDRAL NEIGHBORS
+				pos - type + 10, 
+				pos - type + 11,
+				findPosFromType(findCellFromCoords(mx, ny, nz),8),
+				findPosFromType(findCellFromCoords(mx, ny, nz),9),
+				findPosFromType(findCellFromCoords(nx, ny, mz),6),
+				findPosFromType(findCellFromCoords(nx, ny, mz),7),
+				findPosFromType(findCellFromCoords(mx, ny, mz),4),
+				findPosFromType(findCellFromCoords(mx, ny, mz),5)
+			]
+		)
+	if (type == 3):
+		mx = (nx - 1) % Nx
+		my = (ny + 1) % Ny
+		mz = (nz + 1) % Nz
+		neighbors = np.array (
+			[
+				# OCTAHEDRAL NEIGHBORS
+				pos - 3, 
+				pos - 2, 
+				pos - 1,
+				findPosFromType(findCellFromCoords(mx, ny, nz),1),
+				findPosFromType(findCellFromCoords(mx, ny, nz),2),
+				findPosFromType(findCellFromCoords(nx, my, nz),0),
+				findPosFromType(findCellFromCoords(nx, my, nz),2),
+				findPosFromType(findCellFromCoords(nx, ny, mz),0),
+				findPosFromType(findCellFromCoords(nx, ny, mz),1),
+				findPosFromType(findCellFromCoords(nx, my, mz),0),
+				findPosFromType(findCellFromCoords(mx, ny, mz),1),
+				findPosFromType(findCellFromCoords(mx, my, nz),2),
+				# TETRAHEDRAL NEIGHBORS
+				pos - type + 9, 
+				pos - type + 11,
+				findPosFromType(findCellFromCoords(nx, my, nz),8),
+				findPosFromType(findCellFromCoords(nx, my, nz),10),
+				findPosFromType(findCellFromCoords(nx, ny, mz),5),
+				findPosFromType(findCellFromCoords(nx, ny, mz),7),
+				findPosFromType(findCellFromCoords(nx, my, mz),4),
+				findPosFromType(findCellFromCoords(nx, my, mz),6)
+			]
+		)
+	return neighbors
+
+def TetNeighbors ( pos : int ) :
+	type = pos % 12
+	nx, ny, nz = findCoordsFromPos(pos)
+	neighbors = np.array([pos - type])
+	if (type == 4):
+		mx = (nx - 1) % Nx
+		my = (ny - 1) % Ny
+		mz = (nz - 1) % Nz
+	if (type == 5):
+		mx = (nx - 1) % Nx
+		my = ny
+		mz = (nz - 1) % Nz
+	if (type == 6):
+		mx = nx
+		my = (ny - 1) % Ny
+		mz = (nz - 1) % Nz
+	if (type == 7):
+		mx = nx
+		my = ny
+		mz = (nz - 1) % Nz
+	if (type == 8):
+		mx = (nx - 1) % Nx
+		my = (ny - 1) % Ny
+		mz = nz
+	if (type == 9):
+		mx = (nx - 1) % Nx
+		my = ny
+		mz = nz
+	if (type == 10):
+		mx = nx
+		my = (ny - 1) % Ny
+		mz = nz
+	if (type == 11):
+		mx = nx
+		my = ny
+		mz = nz
+	neighbors = np.append(
+		neighbors, 
+		[
+			findPosFromType(findCellFromCoords(mx, my, nz),1), 
+			findPosFromType(findCellFromCoords(mx, ny, mz),2), 
+			findPosFromType(findCellFromCoords(nx, my, mz),3)
+		]
+	)
+	return neighbors
